5e070694556c13c76a4cb6f9b68dda63988cf20f
Frank Adolfo Estela Rodas <84161066+FrankER21@users.noreply.github.com>
2021-09-18 22:42:41 -0500

    JS

---
+++ Semana4/Dia3/02-aritmetica.js
@@ -0,0 +1,22 @@
+// desde ahora usaremos let
+let a = 100;
+
+let b = 4;
+
+let suma = a + b;
+console.log("Esto es una suma: ",suma);
+
+// puedo poner directamente código de JS dentro del cosole.log(para ver su resultado)
+console.log(1000 + 50);
+
+let resta = a - b;
+console.log("Resta", resta);
+
+let multiplicacion = a * b;
+console.log("Multiplicación: ", multiplicacion);
+
+let division = a / b;
+console.log("Division: ", division);
+
+let residuo = 105 % 4;
+console.log("Modulo o residuo: ", residuo);
